2fc18ed21c3a5c9b678fd2b78c5c58b299c60507
Hayato Ueki <h14170004a@yahoo.co.jp>
2023-11-19 18:20:58 +0900

    create todolist api

---
+++ ToDoListAPI/ToDoListAPI/Controllers/ToDoListController.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using ToDoListAPI.Infrastracture.DB.SQLServer;
+using ToDoListAPI.Models;
+
+namespace ToDoListAPI.Controllers
+{
+    [ApiController]
+    [Route("api/todolist")]
+    public class ToDoListController : Controller
+    {
+        private readonly ClassworkContext _dbContext;
+
+        public ToDoListController(ClassworkContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult ListToDoLists()
+        {
+            FormattableString select = $@"
+SELECT
+  id
+  ,title
+  ,detail
+  ,place
+  ,deadline
+  ,remarks
+  ,created_at
+  ,updated_at
+  ,deleted_at 
+FROM 
+  to_do_list 
+WHERE 
+  deleted_at is null";
+
+            IEnumerable<ToDoList> record;
+
+            try
+            {
+                record = _dbContext.ToDoLists.FromSql(select);
+                // EFの機能でSQL使用しない場合
+                // record = _dbContext.ToDoLists.ToArray();
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok(record);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetToDoList(int id)
+        {
+            FormattableString select = $@"
+SELECT
+  id
+  ,title
+  ,detail
+  ,place
+  ,deadline
+  ,remarks
+  ,created_at
+  ,updated_at
+  ,deleted_at 
+FROM 
+  to_do_list 
+WHERE 
+      id = {id}
+  AND deleted_at is null
+";
+
+            ToDoList? record;
+            try
+            {
+                record = _dbContext.ToDoLists.FromSql(select).FirstOrDefault();
+                // EFの機能でSQL使用しない場合
+                // record = _dbContext.ToDoLists.FindAsync(id);
+                if (record == null)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+
+            return Ok(record);
+        }
+
+        [HttpPost]
+        public IActionResult CreateToDoList([FromBody] ToDoList param)
+        {
+            int count;
+            try
+            {
+                ToDoList toDoList = new ToDoList()
+                {
+                    Title = param.Title,
+                    Detail = param.Detail,
+                    Place = param.Place,
+                    Deadline = param.Deadline,
+                    Remarks = param.Remarks,
+                    CreatedAt = DateTime.Now
+                };
+
+                _dbContext.ToDoLists.Add(toDoList);
+                count = _dbContext.SaveChanges();
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok(count);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateToDoList([FromBody] ToDoList param, int id)
+        {
+            int updateCount = 0;
+            try
+            {
+                var toDoList = _dbContext.ToDoLists.Single(x => x.Id == id);
+                toDoList.Title = param.Title;
+                toDoList.Detail = param.Detail;
+                toDoList.Place = param.Place;
+                toDoList.Deadline = param.Deadline;
+                toDoList.Remarks = param.Remarks;
+                toDoList.UpdatedAt = DateTime.Now;
+
+                int count = _dbContext.SaveChanges();
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok(updateCount);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteToDoList(int id)
+        {
+            int deleteCount = 0;
+            try
+            {
+                var toDoList = _dbContext.ToDoLists.Single(x => x.Id == id);
+                toDoList.DeletedAt = DateTime.Now;
+                // 物理削除の場合
+                //_dbContext.ToDoLists.Remove(toDoList);
+
+                deleteCount = _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok(deleteCount);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteToDoList([FromBody] int[] ids)
+        {
+            int deleteCount = 0;
+            try
+            {
+                deleteCount = _dbContext.ToDoLists.Where(item => ids.Contains(item.Id)).ExecuteUpdate(w => w.SetProperty(x => x.DeletedAt, DateTime.Now));
+                // 物理削除の場合
+                //var toDoList = _dbContext.ToDoLists.Where(item => ids.Contains(item.Id));
+                //_dbContext.ToDoLists.RemoveRange(toDoList);
+                //deleteCount = _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok(deleteCount);
+        }
+    }
+}
---
+++ ToDoListAPI/ToDoListAPI/Models/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoListAPI.Models;
+
+public partial class ToDoList
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Detail { get; set; }
+
+    public string? Place { get; set; }
+
+    public DateTime? Deadline { get; set; }
+
+    public string? Remarks { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public virtual ICollection<ToDoListUser> ToDoListUsers { get; set; } = new List<ToDoListUser>();
+}

79d05f2f6a99f0bb14f15468abc6d4c5630a5868
Hayato Ueki <h14170004a@yahoo.co.jp>
2023-11-25 23:58:30 +0900

    コメント追加、README追記

--- ToDoListAPI/ToDoListAPI/DTO/ToDoListResponse.cs
+++ ToDoListAPI/ToDoListAPI/DTO/ToDoListResponse.cs
@@ -1,22 +1,29 @@
 ﻿namespace ToDoListAPI.DTO
 {
+    /// <summary>
+    /// レスポンス用DTO
+    /// </summary>
     public class ToDoListResponse
     {
+        /// <summary>ID</summary>
         public int Id { get; set; }
 
+        /// <summary>タイトル</summary>
         public string Title { get; set; } = null!;
 
+        /// <summary>詳細</summary>
         public string? Detail { get; set; }
 
+        /// <summary>場所</summary>
         public string? Place { get; set; }
 
+        /// <summary>期限</summary>
         public DateTime? Deadline { get; set; }
 
+        /// <summary>備考</summary>
         public string? Remarks { get; set; }
 
-        public DateTime? ExcessTime { get; set; }
-
-
+        /// <summary>期限切れ判定</summary>
         public bool IsOverdue {  get; set; }
     }
 }
